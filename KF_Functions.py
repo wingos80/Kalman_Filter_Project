fixed a major bug!! Turns out the signs of the biases (The IMU biases) in the state equations were wrong!!! Also, turns out the nose input matrix (G matrix) also was wrong in signs and in elements!!

KF_Functions.py
@@ -7,7 +7,6 @@
 ########################################################################
 import numpy as np
 
-
 def rk4(fn, xin, uin, t):
     """
     4th order Runge-Kutta method for solving ODEs
@@ -83,6 +82,7 @@ def kf_calc_f(t, X, U):
 
     # saving the individual state and input names to make the code more readable
     x, y, z, u, v, w, phi, theta, psi, Wx, Wy, Wz = X[0], X[1], X[2], X[3], X[4], X[5], X[6], X[7], X[8], X[9], X[10], X[11]
+    Lx, Ly, Lz, Lp, Lq, Lr = X[12], X[13], X[14], X[15], X[16], X[17]
     Ax, Ay, Az, p, q, r = U[0], U[1], U[2], U[3], U[4], U[5]
     #####################################################   
     ## System dynamics go here
@@ -98,12 +98,12 @@ def kf_calc_f(t, X, U):
     Xdot[0] = A*cos_psi - B*sin_psi + Wx
     Xdot[1] = A*sin_psi + B*cos_psi + Wy
     Xdot[2] = -u*sin_theta + (v*sin_phi + w*cos_phi)*cos_theta + Wz
-    Xdot[3] = Ax - g*sin_theta + r*v - q*w
-    Xdot[4] = Ay + g*cos_theta*sin_phi + p*w - r*u
-    Xdot[5] = Az + g*cos_theta*cos_phi + q*u - p*v
-    Xdot[6] = p + q*sin_phi*tan_theta + r*cos_phi*tan_theta
-    Xdot[7] = q*cos_phi - r*sin_phi
-    Xdot[8] = q*sin_phi/cos_theta + r*cos_phi/cos_theta
+    Xdot[3] = (Ax - Lx) - g*sin_theta + (r - Lr)*v - (q - Lq)*w
+    Xdot[4] = (Ay - Ly) + g*cos_theta*sin_phi + (p - Lp)*w - (r - Lr)*u
+    Xdot[5] = (Az - Lz)+ g*cos_theta*cos_phi + (q - Lq)*u - (p - Lp)*v
+    Xdot[6] = (p - Lp) + (q - Lq)*sin_phi*tan_theta + (r - Lr)*cos_phi*tan_theta
+    Xdot[7] = (q - Lq)*cos_phi - (r - Lr)*sin_phi
+    Xdot[8] = (q - Lq)*sin_phi/cos_theta + (r - Lr)*cos_phi/cos_theta
     Xdot[9:] = 0
 
     return Xdot
@@ -136,6 +136,7 @@ def kf_calc_Fx(t, X, U):
     
     # saving the individual state and input names to make the code more readable
     x, y, z, u, v, w, phi, theta, psi, Wx, Wy, Wz = X[0], X[1], X[2], X[3], X[4], X[5], X[6], X[7], X[8], X[9], X[10], X[11]
+    Lx, Ly, Lz, Lp, Lq, Lr = X[12], X[13], X[14], X[15], X[16], X[17]
     Ax, Ay, Az, p, q, r = U[0], U[1], U[2], U[3], U[4], U[5]
 
     #####################################################
@@ -178,38 +179,38 @@ def kf_calc_Fx(t, X, U):
     DFx[2,11] = 1
 
     # F4 derivatives
-    DFx[3,4] = r
-    DFx[3,5] = -q
+    DFx[3,4] = (r - Lr)
+    DFx[3,5] = -(q - Lq)
     DFx[3,7] = -g*cos_theta
-    DFx[3,12:] = np.array([1, 0, 0, 0, -w, v], dtype=object)
+    DFx[3,12:] = np.array([-1, 0, 0, 0, w, -v], dtype=object)
 
     # F5 derivatives
-    DFx[4,3] = -r
-    DFx[4,5] = p
+    DFx[4,3] = -(r - Lr)
+    DFx[4,5] = (p - Lp)
     DFx[4,6] = g*cos_phi*cos_theta
-    DFx[4,12:] = np.array([0, 1, 0, w, 0, -u], dtype=object)
+    DFx[4,12:] = np.array([0, -1, 0, -w, 0, u], dtype=object)
 
     # F6 derivatives
-    DFx[5,3] = q
-    DFx[5,4] = -p
+    DFx[5,3] = (q - Lq)
+    DFx[5,4] = -(p - Lp)
     DFx[5,6] = -g*cos_theta*sin_phi
     DFx[5,7] = -g*sin_theta*cos_phi
-    DFx[5,12:] = np.array([0, 0, 1, -v, u, 0], dtype=object)
+    DFx[5,12:] = np.array([0, 0, -1, v, -u, 0], dtype=object)
     # F7 derivatives
-    DFx[6,6] = q*cos_phi*tan_theta - r*sin_phi*tan_theta
-    DFx[6,7] = (q*sin_phi + r*cos_phi)/(cos_theta**2)
-    DFx[6,15:] = np.array([1, sin_phi*tan_theta, cos_phi*tan_theta], dtype=object)
+    DFx[6,6] = (q - Lq)*cos_phi*tan_theta - (r - Lr)*sin_phi*tan_theta
+    DFx[6,7] = ((q - Lq)*sin_phi + (r - Lr)*cos_phi)/(cos_theta**2)
+    DFx[6,15:] = np.array([-1, -sin_phi*tan_theta, -cos_phi*tan_theta], dtype=object)
 
     # F8 derivatives
-    DFx[7,6] = -q*sin_phi - r*cos_phi
-    DFx[7,16] = cos_phi
-    DFx[7,17] = -sin_phi
+    DFx[7,6] = -(q - Lq)*sin_phi - (r - Lr)*cos_phi
+    DFx[7,16] = -cos_phi
+    DFx[7,17] = sin_phi
 
     # F9 derivatives
-    DFx[8,6] = q*cos_phi/cos_theta - r*sin_phi/cos_theta
-    DFx[8,7] = (q*sin_phi*sin_theta + r*cos_phi*sin_theta)/(cos_theta**2)
-    DFx[8,16] = sin_phi/cos_theta
-    DFx[8,17] = cos_phi/cos_theta
+    DFx[8,6] = (q - Lq)*cos_phi/cos_theta - (r - Lr)*sin_phi/cos_theta
+    DFx[8,7] = ((q - Lq)*sin_phi*sin_theta + (r - Lr)*cos_phi*sin_theta)/(cos_theta**2)
+    DFx[8,16] = -sin_phi/cos_theta
+    DFx[8,17] = -cos_phi/cos_theta
 
     return DFx
         
@@ -364,6 +365,7 @@ def kf_calc_Hx(t, X, U):
     # H11 derivatives
     DHx[10,3] =-w/(u**2 + w**2)
     DHx[10,5] = u/(u**2 + w**2)
+
     # H12 derivatives
     DHx[11,3] = -u*v/(np.sqrt(u**2 + w**2)*(V**2))
     DHx[11,4] = (u**2 + w**2)/(V**2)

Kalman_Filter.py
@@ -15,10 +15,8 @@ from KF_Functions import *
 from Kalman_Filter_Class import *
 from Plotter import *
 
-
 np.random.seed(7)                            # Set random seed for reproducibility
 sns.set(style = "darkgrid")                  # Set seaborn style    
-
 
 ########################################################################
 ## Data I/O managing
@@ -58,18 +56,18 @@ figpath         = 'figs/'                    # direction for printed figures
 ## U : numpy.ndarray (m,1)
 ##     input vector, U = [Ax, Ay, Az, p, q, r]^T
 ########################################################################
-E_x_0       = np.zeros([18,1])                                              # initial estimate of optimal value of x_k1_k1
+E_x_0       = np.zeros([18,1])                                              # initial estimate of x_k1_k1
 E_x_0[3:9]  = Z[3:9, 0].reshape(6,1)                                        # initial estimate of velocity and flight angles
 E_x_0[9:12] = np.array([[2], [-8], [1]])                                    # initial estimate of Wind velocities
 E_x_0[12:]  = np.array([[0.02], [0.02], [0.02], [0.003], [0.003], [0.003]]) # initial estimate of lambda
 
-B           = np.zeros([18,6])               # input matrix, TODO: check if this is correct
+B           = np.zeros([18,6])               # input matrix
 
 # Initial estimate for covariance matrix
-std_x_0 = 1.1                                    # initial standard deviation of state prediction error
+std_x_0 = 1                                     # initial standard deviation of state prediction error
 std_x_1 = 1*10**(-3)                            # initial standard deviation of state prediction error
 std_x_2 = 1*10**(+1)                            # initial standard deviation of state prediction error
-P_stds  = [std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2, std_x_0**2]
+P_stds  = [std_x_1, std_x_1, std_x_1, std_x_1, std_x_1, std_x_1, std_x_1, std_x_1, std_x_1, std_x_0, std_x_0, std_x_0, std_x_0, std_x_0, std_x_0, std_x_0, std_x_0, std_x_0]
 
 # System noises, all noise are white (unbiased and uncorrelated in time)
 std_b_x = 0.02                                  # standard deviation of accelerometer x measurement noise
@@ -78,12 +76,24 @@ std_b_z = 0.02                                  # standard deviation of accelero
 std_b_p = 0.003                                 # standard deviation of rate gyro p measurement noise 
 std_b_q = 0.003                                 # standard deviation of rate gyro q measurement noise  
 std_b_r = 0.003                                 # standard deviation of rate gyro r measurement noise  
-Q_stds  = [std_b_x**2, std_b_y**2, std_b_z**2, std_b_p**2, std_b_q**2, std_b_r**2]
+Q_stds  = [std_b_x, std_b_y, std_b_z, std_b_p, std_b_q, std_b_r]
 
+u, v, w = E_x_0[3], E_x_0[4], E_x_0[5]
+phi, theta, psi = E_x_0[6], E_x_0[7], E_x_0[8]
+sin_phi_tan_theta = np.sin(phi) * np.tan(theta)
+cos_phi_tan_theta = np.cos(phi) * np.tan(theta)
+cos_phi = np.cos(phi)
+sin_phi = np.sin(phi)
+sin_phi_div_cos_theta = np.sin(phi) / np.cos(theta)
+cos_phi_div_cos_theta = np.cos(phi) / np.cos(theta)
+# TEMPORARY TEST FOR THE SYSTEM NOISE MATRIX, G is time variant so i need to make it update every time with the new speeds
 G       = np.zeros([18, 6])                      # system noise matrix
-G[3:6, 0:3] = np.eye(3)                                                                   # accelerometer noise
-G[3:6, 3:]  = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])                              # rate gyro noise
-G[6:9, 3:]  = np.array([[1, 1, 1], [0, 1, -1], [0, 1, 1]])                                # rate gyro noise
+G[3:6, 0:3] = -np.eye(3)                                                                        # accelerometer noise (has a negative because the Ax in the model should be Am MINUS bias MINUS noise!!!!)
+G[3:9, 3:]  = np.array([[0, w, -v], [-w, 0, u], [v, -u, 0], [1, -sin_phi_tan_theta, -cos_phi_tan_theta], [0, -cos_phi, sin_phi], [0, -sin_phi_div_cos_theta, -cos_phi_div_cos_theta]])  # rate gyro noise
+
+# G       = np.zeros([18, 6])                      # system noise matrix
+# G[3:6, 0:3] = -np.eye(3)                                                                        # accelerometer noise (has a negative because the Ax in the model should be Am MINUS bias MINUS noise!!!!)
+# G[3:9, 3:]  = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0], [1, 1, 1], [0, 1, -1], [0, 1, 1]])  # rate gyro noise
 
 # Measurement noise statistics, all noise are white (unbiased and uncorrelated in time)
 std_gps_x = 2.5                                 # standard deviation of GPS x position measurement noise
@@ -100,7 +110,7 @@ std_ads_v = 0.1                                 # standard deviation of air data
 std_ads_alpha = 0.1                             # standard deviation of air data sensors alpha measurement noise
 std_ads_beta = 0.1                              # standard deviation of air data sensors beta measurement noise
 
-R_stds   = [std_gps_x**2, std_gps_y**2, std_gps_z**2, std_gps_u**2, std_gps_v**2, std_gps_w**2, std_gps_phi**2, std_gps_theta**2, std_gps_psi**2, std_ads_v**2, std_ads_alpha**2, std_ads_beta**2]
+R_stds   = [std_gps_x, std_gps_y, std_gps_z, std_gps_u, std_gps_v, std_gps_w, std_gps_phi, std_gps_theta, std_gps_psi, std_ads_v, std_ads_alpha, std_ads_beta]
 
 ########################################################################
 ## Run the Kalman filter
@@ -122,8 +132,8 @@ for k in range(N):
     if k % 100 == 0:
         tonc = time.time()
         print(f'Sample {k} of {N} ({k/N*100:.3f} %), time elapsed: {tonc-tic:.2f} s')
-        print(f'    Current estimate of system states:\n{kalman_filter.x_k1_k1}\n')
-    
+        # print(f'    Current estimate of system states:\n{kalman_filter.x_k1_k1}\n')
+    bing = time.time()
     # Picking out the k-th entry in the input and measurement vectors
     U_k = U[:,k]  
     Z_k = Z[:,k]
@@ -137,55 +147,89 @@ for k in range(N):
 
     # Once converged, update the state and state covariances estimates
     kalman_filter.update(k)
-    
 
+    bong = time.time()
+#     print(f'    Iteration time: {bong-bing:.5f} s')
+
+                                        
 toc = time.time()
 
 print(f'Elapsed time: {toc-tic:.5f} s')
+
+
+########################################################################
+## Saving the results
+########################################################################
+
+file_Xs   = filename.split('_measurements.')[0] + '_Xs-kf_measurements.csv'
+file_Zs   = filename.split('_measurements.')[0] + '_Zs-kf_measurements.csv'
+result_file_Xs = open(file_Xs, "w")
+result_file_Zs = open(file_Zs, "w")
+
+# writing the column headings in the resulte file, ADS stands for airdata sensors
+result_file_Xs.write(f"x_kf, y_kf, z_kf, u_kf, v_kf, w_kf, phi_kf, theta_kf, psi_kf, Wx_kf, Wy_kf, Wz_kf, Lx_kf, Ly_kf, Lz_kf, Lp_kf, Lq_kf, Lr_kf")
+result_file_Zs.write(f"x_gps, y_gps, z_gps, u_gps, v_gps, w_gps, phi_gps, theta_gps, psi_gps, v_ads, alpha_ads, beta_ads")
+
+Xs, Zs = kalman_filter.XX_k1_k1, kalman_filter.ZZ_pred
+# writing every entry of the kalman filter states to the result file
+for k in range(N):
+    result_file_Xs.write(f"{Xs[0,k]}, {Xs[1,k]}, {Xs[2,k]}, {Xs[3,k]}, {Xs[4,k]}, {Xs[5,k]}, {Xs[6,k]}, {Xs[7,k]}, {Xs[8,k]}, {Xs[9,k]}, {Xs[10,k]}, {Xs[11,k]}, {Xs[12,k]}, {Xs[13,k]}, {Xs[14,k]}, {Xs[15,k]}, {Xs[16,k]}, {Xs[17,k]}")
+    result_file_Zs.write(f"{Zs[0,k]}, {Zs[1,k]}, {Zs[2,k]}, {Zs[3,k]}, {Zs[4,k]}, {Zs[5,k]}, {Zs[6,k]}, {Zs[7,k]}, {Zs[8,k]}, {Zs[9,k]}, {Zs[10,k]}, {Zs[11,k]}")
+
+    
+# Saving the kalman filtered measurements (predicts)
+xyz_kf             = Xs[0:3]          # Predicted position from KF
+Winds              = Xs[9:12]         # Predicted alpha from KF
+Winds_covariances  = kalman_filter.PP_k1_k1[9:12]         # Predicted alpha covariance from KF
+new_lambdas        = Xs[12:]          # Predicted lambda from KF
+lambda_covariances = kalman_filter.PP_k1_k1[12:]          # Predicted lambda covariance from KF
+kf_u               = Zs[3]             # Predicted u from KF
+unkf_u             = Z[3]
 
 ########################################################################
 ## Plotting some of the results
 ########################################################################
 
-# Saving the kalman filtered measurements (predicts)
-xyz_kf             = kalman_filter.XX_k1_k1[0:3]          # Predicted position from KF
-Winds              = kalman_filter.XX_k1_k1[9:12]         # Predicted alpha from KF
-Winds_covariances  = kalman_filter.PP_k1_k1[9:12]         # Predicted alpha covariance from KF
-new_lambdas        = kalman_filter.XX_k1_k1[12:]          # Predicted lambda from KF
-lambda_covariances = kalman_filter.PP_k1_k1[12:]          # Predicted lambda covariance from KF
-kf_u               = kalman_filter.ZZ_pred[3]             # Predicted u from KF
-unkf_u             = Z[3]
-
 
 x      = dt*np.arange(0, N, 1)
-ys     = {'raw u\'s': [unkf_u, 0.9], 
-          'kf u\'s': [kf_u, 0.9]}
-make_plots(x, [ys], 'raw and kalman-filtered u velocities', 'Time [s]', ['u [m/s]'], printfigs)
+# ys     = {'raw u\'s': [unkf_u, 0.9], 
+#           'kf u\'s': [kf_u, 0.9]}
+# make_plots(x, [ys], 'raw and kalman-filtered u velocities', 'Time [s]', ['u [m/s]'], printfigs)
 
-ys1 = {'Wind x': [Winds[0], 0.9],
-      'Wind y': [Winds[1], 0.9],
-      'Wind z': [Winds[2], 0.9]}
-ys2 = {'Wind x variance': [Winds_covariances[0], 0.9],
-      'Wind y variance': [Winds_covariances[1], 0.9],
-      'Wind z variance': [Winds_covariances[2], 0.9]}
+print(f' shapes of XX_k1_k1 and ZZ_pred: {Xs.shape}, {kalman_filter.ZZ_pred.shape}')
+dX_Z = Xs[:9,:] - kalman_filter.ZZ_pred[:9,:]
+ys = {'u from Z': [kalman_filter.ZZ_pred[3,:], 0.3],
+      'v from Z': [kalman_filter.ZZ_pred[4,:], 0.3],
+      'w from Z': [kalman_filter.ZZ_pred[5,:], 0.3],
+      'u from X': [Xs[3,:], 0.9],
+      'v from X': [Xs[4,:], 0.9],
+      'w from X': [Xs[5,:], 0.9],}
+make_plots(x, [ys], 'Difference between predicted and measured states', 'Time [s]', ['Difference [m/s]'], printfigs)
 
-make_plots(x, [ys1, ys2], 'Wind over time', 'Time [s]', ['Wind [m/s]', 'Wind variance [m2/s2]'], printfigs, log=1)
+# ys1 = {'Wind x': [Winds[0], 0.9],
+#       'Wind y': [Winds[1], 0.9],
+#       'Wind z': [Winds[2], 0.9]}
+# ys2 = {'Wind x variance': [Winds_covariances[0], 0.9],
+#       'Wind y variance': [Winds_covariances[1], 0.9],
+#       'Wind z variance': [Winds_covariances[2], 0.9]}
 
-ys1 = {'lambda Ax': [new_lambdas[0], 0.9],
-        'lambda Ay': [new_lambdas[1], 0.9],
-        'lambda Az': [new_lambdas[2], 0.9]}
-ys2 = {'lambda Ax variance': [lambda_covariances[0], 0.9],
-        'lambda Ay variance': [lambda_covariances[1], 0.9],
-        'lambda Az variance': [lambda_covariances[2], 0.9]}
-make_plots(x, [ys1, ys2], 'acceleration biases variance over time', 'Time [s]', ['Bias [m/s2]', 'Variance [m2/s4]'], printfigs, log=1)
+# make_plots(x, [ys1, ys2], 'Wind over time', 'Time [s]', ['Wind [m/s]', 'Wind variance [m2/s2]'], printfigs, log=1)
 
-ys1 = {'lambda p': [new_lambdas[3], 0.9],
-        'lambda q': [new_lambdas[4], 0.9],
-        'lambda r': [new_lambdas[5], 0.9]}
-ys2 = {'lambda p variance': [lambda_covariances[3], 0.9],
-        'lambda q variance': [lambda_covariances[4], 0.9],
-        'lambda r variance': [lambda_covariances[5], 0.9]}
-make_plots(x, [ys1, ys2], 'angle bias variances over time', 'Time [s]', ['Bias [rad/s]', 'Variance [rad2/s2]'], printfigs, log=1)
+# ys1 = {'lambda Ax': [new_lambdas[0], 0.9],
+#         'lambda Ay': [new_lambdas[1], 0.9],
+#         'lambda Az': [new_lambdas[2], 0.9]}
+# ys2 = {'lambda Ax variance': [lambda_covariances[0], 0.9],
+#         'lambda Ay variance': [lambda_covariances[1], 0.9],
+#         'lambda Az variance': [lambda_covariances[2], 0.9]}
+# make_plots(x, [ys1, ys2], 'acceleration biases variance over time', 'Time [s]', ['Bias [m/s2]', 'Variance [m2/s4]'], printfigs, log=1)
+
+# ys1 = {'lambda p': [new_lambdas[3], 0.9],
+#         'lambda q': [new_lambdas[4], 0.9],
+#         'lambda r': [new_lambdas[5], 0.9]}
+# ys2 = {'lambda p variance': [lambda_covariances[3], 0.9],
+#         'lambda q variance': [lambda_covariances[4], 0.9],
+#         'lambda r variance': [lambda_covariances[5], 0.9]}
+# make_plots(x, [ys1, ys2], 'angle bias variances over time', 'Time [s]', ['Bias [rad/s]', 'Variance [rad2/s2]'], printfigs, log=1)
 
 ys = {'Iterations taken by IEKF': [kalman_filter.itr_counts, 0.9]}
 make_plots(x, [ys], 'Iterations taken by IEKF', 'Time [s]', ['Iterations'], printfigs)
@@ -197,7 +241,7 @@ fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
 ax.scatter(xyz[0], xyz[1], xyz[2], c='r', marker='o', label='Simulated', s=1)
-ax.scatter(xyz_kf[0], xyz_kf[1], xyz_kf[2], c='b', marker='o', label='Kalman filtered', s=1)
+ax.scatter(xyz_kf[0], xyz_kf[1], xyz_kf[2], c='b', marker='o', label='Kalman filtered (xyz state)', s=1)
 plt.title(f'{filename}\'s reconstructed flight path', fontsize = 18)
 plt.xlabel('x (m)', fontsize = 14)
 plt.ylabel('y (m)', fontsize = 14)

Kalman_Filter_Class.py
@@ -148,6 +148,21 @@ class IEKF:
 
         # x(k+1|k) (prediction)
         self.t, self.x_k1_k   = self.integrator(self.f, self.x_k1_k1, U_k, [self.t_k, self.t_k1])   # add in U_k vector
+        
+        u, v, w = self.x_k1_k[3], self.x_k1_k[4], self.x_k1_k[5]
+        phi, theta, psi = self.x_k1_k[6], self.x_k1_k[7], self.x_k1_k[8]
+        sin_phi_tan_theta = np.sin(phi) * np.tan(theta)
+        cos_phi_tan_theta = np.cos(phi) * np.tan(theta)
+        cos_phi = np.cos(phi)
+        sin_phi = np.sin(phi)
+        sin_phi_div_cos_theta = np.sin(phi) / np.cos(theta)
+        cos_phi_div_cos_theta = np.cos(phi) / np.cos(theta)
+
+        # TEMPORARY TEST FOR THE SYSTEM NOISE MATRIX, G is time variant so i need to make it update every time with the new speeds
+        G       = np.zeros([18, 6])                      # system noise matrix
+        G[3:6, 0:3] = -np.eye(3)                                                                        # accelerometer noise (has a negative because the Ax in the model should be Am MINUS bias MINUS noise!!!!)
+        G[3:9, 3:]  = np.array([[0, w, -v], [-w, 0, u], [v, -u, 0], [1, -sin_phi_tan_theta, -cos_phi_tan_theta], [0, -cos_phi, sin_phi], [0, -sin_phi_div_cos_theta, -cos_phi_div_cos_theta]])  # rate gyro noise
+        self.G = G
 
         # Calc Jacobians, Phi(k+1, k), and Gamma(k+1, k)
         F_jacobian  = self.Fx(0, self.x_k1_k, U_k)
@@ -203,10 +218,9 @@ class IEKF:
             Measurement vector for the k-th time step
         """
         self.itr +=1
-        self.eta1 = self.eta2
-
+        eta1 = self.eta2
         # Construct the Jacobian H = d/dx(h(x))) with h(x) the observation model transition matrix 
-        H_jacobian  = self.Hx(0, self.eta1, U_k)
+        H_jacobian  = self.Hx(0, eta1, U_k)
         
         # Check observability of state
         if (k == 0 and self.itr == 1):
@@ -217,22 +231,17 @@ class IEKF:
                 print(f'The current state is not observable; rank of Observability Matrix is {rankHF}, should be {self.n}\n')
 
         # Observation and observation error predictions
-        self.z_k1_k      = self.h(0, self.eta1, U_k)                            # prediction of observation (for validation)   
+        self.z_k1_k      = self.h(0, eta1, U_k)                            # prediction of observation (for validation)   
         P_zz        = H_jacobian@self.P_k1_k@H_jacobian.transpose() + self.R    # covariance matrix of observation error (for validation)   
-        
-        # Raise exception in case the covariance matrix is too small    
-        try:
-            self.std_z       = np.sqrt(P_zz.diagonal())          # standard deviation of observation error (for validation)    
-        except:
-            self.std_z       = np.zeros([self.nm, 1])                 # standard deviation of observation error (for validation)  
+        self.std_z       = np.sqrt(P_zz.diagonal())          # standard deviation of observation error (for validation)    
 
         # K(k+1) (gain), Kalman Gain
         Kalman_Gain             = self.P_k1_k@H_jacobian.transpose()@np.linalg.inv(P_zz)
     
         # New observation
         temp = np.reshape(Z_k, (self.nm,1))                  # Need to reshape this Z array to a column vector
-        eta2        = self.x_k1_k + Kalman_Gain@(temp - self.z_k1_k - H_jacobian@(self.x_k1_k - self.eta1))
-        self.err    = np.linalg.norm(eta2-self.eta1)/np.linalg.norm(self.eta1)  # difference in updated state estimate 
+        eta2        = self.x_k1_k + Kalman_Gain@(temp - self.z_k1_k - H_jacobian@(self.x_k1_k - eta1))
+        self.err    = np.linalg.norm(eta2-eta1)/np.linalg.norm(eta1)  # difference in updated state estimate 
                                                                                      # and previous state estimate
         self.H_jacobian  = H_jacobian
         self.Kalman_Gain = Kalman_Gain
@@ -278,9 +287,9 @@ class IEKF:
         Check if the IEKF has converged
         """
         bool_val = self.err > self.epsilon and self.itr < self.max_itr
-        if self.itr > self.max_itr:
-            print('Maximum number of iterations reached')
-            print(f'Delta eta: {self.err}, epsilon: {self.epsilon}')
+        if self.itr >= self.max_itr:
+            print('\nMaximum number of iterations reached, test 2')
+            print(f'Delta eta: {self.err}, epsilon: {self.epsilon}\n')
         return bool_val
     
 

test.py
@@ -1,39 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
-    return x
+a = "hello.piii"
 
-t = 0
-dt = 1
-x0 = 1
-x0_si = x0
-
-
-N = 5
-x = []
-x_exact = []
-x_si = []
-
-x.append(x0)
-x_exact.append(np.exp(t))
-x_si.append(x0_si)
-for i in range(N):
-    t+=dt
-    x_exact.append(np.exp(t))
-    
-    x0 = x0+ dt*f(x0)
-    x0_si_temp = x0_si + dt*f(x0_si)
-    x0_si = x0_si + dt*f(x0_si_temp)
-
-    
-    x.append(x0)
-    x_si.append(x0_si)
-
-
-plt.plot(x, label='forward euler simple')
-plt.plot(x_exact, label='exact')
-plt.plot(x_si, label='si euler')
-plt.grid()
-plt.legend()
-plt.show()
+print(a.split('o.'))