turns out the angle conversion in data_regenetaion.py was wrong, should not need to mutiply the angles by pi/180 to make the angles to raidans: they were already in radians

Data_Regeneration.py
@@ -33,16 +33,17 @@ for filename in os.listdir(os.getcwd()):
         train_data = genfromtxt(filename, delimiter=',').T
         train_data = train_data[:, 1:]
 
+        # all angles should be in radians
         times       = train_data[0]
-        phi        = train_data[1]*np.pi/180
-        theta      = train_data[2]*np.pi/180
-        psi        = train_data[3]*np.pi/180
+        phi        = train_data[1]
+        theta      = train_data[2]
+        psi        = train_data[3]
         p          = train_data[4]
         q          = train_data[5]
         r          = train_data[6]
         vtas       = train_data[7]
-        alpha      = train_data[9]*np.pi/180
-        beta       = train_data[10]*np.pi/180
+        alpha      = train_data[9]
+        beta       = train_data[10]
         ax         = train_data[12]
         ay         = train_data[13]
         az         = train_data[14]
@@ -84,23 +85,29 @@ for filename in os.listdir(os.getcwd()):
         for k in range(len(times)-1):
             if k % 500 == 0:
                 print(f"Time step: {k} of {len(times)}")
-            t_vector     = [times[k], times[k+1]]
-            t_vector, X = rk4(kf_calc_f, X, U, t_vector)       # rk4 to integrate state vector to next time step
+            dt = times[k+1] - times[k]
+            xyz[:, k+1]   = xyz[:, k] + np.array([dt*u_n[k],dt*v_n[k],dt*w_n[k]])
+            gps_t[:3,k+1] = xyz[:, k+1]
 
-            # Picking out select states to re-create flight path, GPS measurements, and airdata measurements
-            xyz[:, k+1]   = X[0:3,0]
-            gps_t[:3,k+1] = X[0:3,0]
+            # t_vector     = [times[k], times[k+1]]
+            # t_vector, X = rk4(kf_calc_f, X, U, t_vector)       # rk4 to integrate state vector to next time step
+
+            # # Picking out select states to re-create flight path, GPS measurements, and airdata measurements
+            # xyz[:, k+1]   = X[0:3,0]
+            # gps_t[:3,k+1] = X[0:3,0]
             
-            X[3:12] = np.array([[u_n[k+1]], [v_n[k+1]], [w_n[k+1]], [phi[k+1]], [theta[k+1]], [psi[k+1]], [Wx], [Wy], [Wz]])
+            # X[3:12] = np.array([[u_n[k+1]], [v_n[k+1]], [w_n[k+1]], [phi[k+1]], [theta[k+1]], [psi[k+1]], [Wx], [Wy], [Wz]])
 
             # Storing the measurements at each time step
             result_file.write(f"{xyz[0,k+1]}, {xyz[1,k+1]}, {xyz[2,k+1]}, {gps_t[0,k+1]}, {gps_t[1,k+1]}, {gps_t[2,k+1]}, {gps_t[3,k+1]}, {gps_t[4,k+1]}, {gps_t[5,k+1]}, {gps_t[6,k+1]}, {gps_t[7,k+1]}, {gps_t[8,k+1]}, {airdata_t[0,k+1]}, {airdata_t[1,k+1]}, {airdata_t[2,k+1]}, {imu_t[0,k+1]}, {imu_t[1,k+1]}, {imu_t[2,k+1]}, {imu_t[3,k+1]}, {imu_t[4,k+1]}, {imu_t[5,k+1]}\n")
             
+
+        
         # Use 3D scatter plot to visualize the airplane position over time
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
 
-        ax.scatter(xyz[0], xyz[1], xyz[2], c='r', marker='o', label='Measured', s=1)
+        ax.scatter(xyz[0], xyz[1], xyz[2], c='r', marker='o', label='normal integrate', s=1)
         plt.title(f'{filename}\'s reconstructed flight path', fontsize = 18)
         plt.xlabel('x (m)', fontsize = 14)
         plt.ylabel('y (m)', fontsize = 14)

Kalman_Filter_Class.py
@@ -48,30 +48,22 @@ class IEKF:
         """
         Set up the system dynamics, output equations, initial guess of system
         state.
-
         Parameters
         ----------
         x_0 : np.array
             Initial guess of system state
-
         f : function
             System dynamics function
-        
         h : function
             Output equation function
-
         Fx : function
             Jacobian of system dynamics function
-
         Hx : function    
             Jacobian of output equation function
-
         B : np.array
             Input matrix
-
         G : np.array    
             Input noise matrix
-
         integrator : function
             selected integration scheme for integrating the system dynamics
         """
@@ -93,6 +85,11 @@ class IEKF:
         # saving the integation scheme chosen
         self.integrator = integrator
 
+        if B.shape[0] != self.n:
+            raise ValueError(f'B matrix dimension 0 must be the same size as the state vector (B.shape[0] = {B.shape[0]}, self.n = {self.n})')
+        if G.shape[0] != self.n:
+            raise ValueError(f'G matrix dimension 0 must be the same size as the state vector (G.shape[0] = {G.shape[0]}, self.n = {self.n})')
+        
         # set up memory vectors
         self.setup_traces()
 
@@ -107,6 +104,7 @@ class IEKF:
         self.STD_x_cor   = np.zeros([self.n, self.N])   # memory for filter state standard deviation
         self.ZZ_pred     = np.zeros([self.nm, self.N])  # memory for filter measurement estimate
         self.STD_z       = np.zeros([self.nm, self.N])  # memory for filter measurement standard deviation
+        self.innovations = np.zeros([self.nm, self.N])  # memory for filter measurement innovations
 
         self.itr_counts = np.zeros([self.N])           # memory for IEKF iteration count
         self.eye_n       = np.eye(self.n)               # identity matrix of size n for use in computations
@@ -115,7 +113,6 @@ class IEKF:
     def setup_covariances(self, P_stds, Q_stds, R_stds):
         """
         Set up the system state and noise covariance matrices
-
         Parameters
         ----------
         P_stds : list
@@ -134,12 +131,17 @@ class IEKF:
 
         self.P_k1_k1 = self.P_0
 
+        if len(P_stds) != self.n:
+            raise ValueError(f'Number of state estimate stds must match number of states (len(P_stds) = {len(P_stds)}, self.n = {self.n})')
+        if len(Q_stds) != self.m:
+            raise ValueError(f'Number of system noise stds must match number of system noise signals (len(Q_stds) = {len(Q_stds)}, self.m = {self.m})')
+        if len(R_stds) != self.nm:
+            raise ValueError(f'Number of measurement noise stds must match number of measurements (len(R_stds) = {len(R_stds)}, self.nm = {self.nm})')
+
 
     def predict_and_discretize(self, U_k):
         """
-        Predict the next state and discretize the system dynamics and output
-        equations
-
+        Predict the next state and discretize the system dynamics and output equations
         Parameters
         ----------
         U_k : np.array
@@ -149,6 +151,8 @@ class IEKF:
         # x(k+1|k) (prediction)
         self.t, self.x_k1_k   = self.integrator(self.f, self.x_k1_k1, U_k, [self.t_k, self.t_k1])   # add in U_k vector
         
+
+        # TEMPORARY TEST FOR THE SYSTEM NOISE MATRIX, G is time variant so i need to make it update every time with the new speeds
         u, v, w = self.x_k1_k[3], self.x_k1_k[4], self.x_k1_k[5]
         phi, theta, psi = self.x_k1_k[6], self.x_k1_k[7], self.x_k1_k[8]
         sin_phi_tan_theta = np.sin(phi) * np.tan(theta)
@@ -158,10 +162,9 @@ class IEKF:
         sin_phi_div_cos_theta = np.sin(phi) / np.cos(theta)
         cos_phi_div_cos_theta = np.cos(phi) / np.cos(theta)
 
-        # TEMPORARY TEST FOR THE SYSTEM NOISE MATRIX, G is time variant so i need to make it update every time with the new speeds
         G       = np.zeros([18, 6])                      # system noise matrix
         G[3:6, 0:3] = -np.eye(3)                                                                        # accelerometer noise (has a negative because the Ax in the model should be Am MINUS bias MINUS noise!!!!)
-        G[3:9, 3:]  = np.array([[0, w, -v], [-w, 0, u], [v, -u, 0], [1, -sin_phi_tan_theta, -cos_phi_tan_theta], [0, -cos_phi, sin_phi], [0, -sin_phi_div_cos_theta, -cos_phi_div_cos_theta]])  # rate gyro noise
+        G[3:9, 3:]  = np.array([[0, w, -v], [-w, 0, u], [v, -u, 0], [1, -sin_phi_tan_theta, -cos_phi_tan_theta], [0, -cos_phi, sin_phi], [0, -sin_phi_div_cos_theta, -cos_phi_div_cos_theta]], dtype=object)  # rate gyro noise
         self.G = G
 
         # Calc Jacobians, Phi(k+1, k), and Gamma(k+1, k)
@@ -184,12 +187,10 @@ class IEKF:
     def check_obs_rank(self, Hx, Fx):
         """
         Check the observability of the state
-
         Parameters
         ----------
         Hx : np.array
             Jacobian of the output equation
-
         Fx : np.array
             Jacobian of the system dynamics
         """
@@ -205,15 +206,13 @@ class IEKF:
         return rankHF
 
 
-    def run_iteration(self, U_k, Z_k, k):
+    def run_one_iteration(self, U_k, Z_k, k):
         """
         Run one iteration of the IEKF
-
         Parameters
         ----------
         U_k : np.array
             Input vector for the k-th time step
-
         Z_k : np.array
             Measurement vector for the k-th time step
         """
@@ -242,15 +241,21 @@ class IEKF:
         temp = np.reshape(Z_k, (self.nm,1))                  # Need to reshape this Z array to a column vector
         eta2        = self.x_k1_k + Kalman_Gain@(temp - self.z_k1_k - H_jacobian@(self.x_k1_k - eta1))
         self.err    = np.linalg.norm(eta2-eta1)/np.linalg.norm(eta1)  # difference in updated state estimate 
-                                                                                     # and previous state estimate
+                                                                                          # and previous state estimate
         self.H_jacobian  = H_jacobian
         self.Kalman_Gain = Kalman_Gain
+        self.z_k1_k      = self.h(0, eta2, U_k)                            # prediction of observation (for validation)   
         self.eta2        = eta2
 
-
-    def update(self, k):
+    def update(self, U_k, k):
         """
         Update the state and state covariance estimates, and store the results
+        Parameters
+        ----------
+        k : int
+            Current iteration step
+        U_k: np.array
+            Input vector for the k-th time step
         """
 
         # Updating the state estimate
@@ -264,31 +269,37 @@ class IEKF:
         P_k1_k1          = (self.eye_n - K@H_j)@self.P_k1_k@(self.eye_n - K@H_j).transpose() + K@self.R@K.transpose()    
         self.std_x_cor   = np.sqrt(P_k1_k1.diagonal())        # standard deviation of state estimation error (for validation)
 
-        # Update to next time step
-        self.t_k         = self.t_k1 
-        self.t_k1        = self.t_k1 + self.dt
-
-        self.P_k1_k1     = P_k1_k1
+        # calculate the kalman filter 'innovation', difference in measured and predicted observation
+        innovation = self.z_k1_k - self.h(0, self.x_k1_k1, U_k)
 
         # Store results, need to flatten the arrays to store in a matrix
+        self.P_k1_k1     = P_k1_k1
         self.ZZ_pred[:,k]    = self.z_k1_k.flatten()              # predicted observation
         self.XX_k1_k1[:,k]   = self.x_k1_k1.flatten()             # estimated state
         self.PP_k1_k1[:,k]   = self.P_k1_k1.diagonal().flatten()  # estimated state covariance (for validation)
         self.STD_x_cor[:,k]  = self.std_x_cor.flatten()           # standard deviation of state estimation error (for validation)
         self.STD_z[:,k]      = self.std_z.flatten()               # standard deviation of observation error (for validation)
+        self.innovations[:,k] = innovation.flatten()               # kalman filter 'innovation'
 
+        # Update to next time step
+        self.t_k         = self.t_k1 
+        self.t_k1        = self.t_k1 + self.dt
         self.itr_counts[k] = self.itr
         self.itr = 0
         self.err = 2*self.epsilon
 
 
-    def not_converged(self):
+    def not_converged(self, k):
         """
         Check if the IEKF has converged
+        Parameters
+        ----------
+        k : int
+            Current iteration step
         """
         bool_val = self.err > self.epsilon and self.itr < self.max_itr
         if self.itr >= self.max_itr:
-            print('\nMaximum number of iterations reached, test 2')
+            print(f'\nMaximum number of iterations reached at step = {k}')
             print(f'Delta eta: {self.err}, epsilon: {self.epsilon}\n')
         return bool_val
     

Plotter.py
@@ -1,73 +1,64 @@
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotter(ax, x, y, ylabel, log=False):
+def plotter(ax, x, y, ylabel, colors=None, log=False):
     """
     Plotter function for general graphs
-
     Parameters
     ----------
     x : np.array
         x-axis vector
-
     y : dictionary
         List of length 2 in each value, 1st element is f(x),
         2nd element is transparency(alpha) of that graph,
         each key is the label for the corresponding graph
-
     title : string
-        Title of the plot
-    
+        Title of the plot    
     xlabel : string
-        Label of the x-axis
-    
+        Label of the x-axis    
     ylabel : string
         Label of the y-axis
-
     save : bool, optional
         Save the figure to a file, by default False        
     """
 
     # fig = plt.figure(figsize=(12,6))
     # ax  = fig.add_subplot()
+    i=0
     for key, value in y.items():
-        ax.plot(x, value[0], label=key, alpha=value[1])
+        ax.plot(x, value[0], color=colors[i] if colors else f'C{i}',label=key, alpha=value[1])
+        i+=1
     if log:
         ax.set_yscale('log')
     ax.grid(True); ax.set_ylabel(ylabel); ax.legend()
     
 
-def make_plots(x, ys, title, xlabel, ylabel, save=False, log=None):
+def make_plots(x, ys, title, xlabel, ylabel, colors=None, save=False, log=None):
     """
-    Make multiple plots in one figure
-    
+    Make multiple plots in one figure    
     Parameters
-    ----------
-    
+    ----------    
     x : np.array
-        x-axis vector
-    
+        x-axis vector    
     ys : list
-        List of dictionaries, each dictionary is a plot
-        
+        List of dictionaries, each dictionary is a plot        
     title : string
-        Title of the plot
-        
+        Title of the plot        
     xlabel : string
-        Label of the x-axis
-        
+        Label of the x-axis        
     ylabel : list
-        List of labels for the y-axis of each subplots
-    
+        List of labels for the y-axis of each subplots    
+    colors : list, optional
+        List of colors for each plot, by default None
     save : bool, optional
-        Save the figure to a file, by default False
-        
+        Save the figure to a file, by default False        
     log : int, optional
         Index of the plot to be plotted in log scale, by default None    
     """
-    fig, ax = plt.subplots(len(ys), 1, figsize=(8, 3), sharex=True)
-    fig.suptitle(title, fontsize = 18);
-    
+    fig, ax = plt.subplots(len(ys), 1, sharex=True)
+    fig.canvas.manager.set_window_title('My Window Title') 
+    plt.subplots_adjust(top=0.965, bottom=0.110, right=0.990, left=0.145)
     if len(ylabel) != len(ys):
         print('---Plot Warning, Number of ylabel must be equal to number of plots')
             
@@ -79,9 +70,16 @@ def make_plots(x, ys, title, xlabel, ylabel, save=False, log=None):
 
     for i, y in enumerate(ys):
         if i == log:
-            plotter(ax[i], x, y, ylabel[i], log=True)
+            plotter(ax[i], x, y, ylabel[i], colors=colors,log=True)
         else:
-            plotter(ax[i], x, y, ylabel[i], log=None)
+            plotter(ax[i], x, y, ylabel[i], colors=colors,log=None)
     
     if save:
-        plt.savefig(title + '.png')
+        plt.savefig(f'figs/{title.replace(" ", "_").replace("-", "_")}.pdf')
+
+# sns.set(style = "darkgrid")                  # Set seaborn style    
+
+# x = np.arange(0,100,1)
+# ys = {'sine': [np.sin(x), 1], 'cosine': [np.cos(x), 1]}
+# make_plots(x, [ys, ys], 'Sine and-Cosine', r'x and $\phi$', [r'$\lambda_{x_r}variance$', r'y $\frac{1}{2}$'], save=True)
+# plt.show()